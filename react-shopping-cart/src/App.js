b2b7d7554a74f99e2b9897bb8e85e79af0c853c7
Grayd-WebDev <grayd2@yandex.ru>
2020-09-23 18:50:08 +0300

    create product component

--- react-shopping-cart/src/App.js
+++ react-shopping-cart/src/App.js
@@ -1,20 +1,32 @@
 // feature 1
-import React from 'react';
+import React, { Component } from "react";
+import ProductList from "./components/product-list";
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header className="App-header">
-      <a href="/">React Shopping Cart</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All rights are reserved
-      </footer>
-    </div>
-  );
+class App extends Component {
+  state = {
+    products: data.products,
+    size: "",
+    sort: "",
+  };
+  render() {
+    return (
+      <div className="grid-container">
+        <header className="App-header">
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <ProductList products={this.state.products}/>
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All rights are reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
