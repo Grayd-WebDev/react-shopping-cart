8d01a9f046d308a9f07d55b0e2391f4795c56580
Grayd-WebDev <grayd2@yandex.ru>
2020-10-16 21:59:22 +0300

    Product service and HOC withProductService

--- react-shopping-cart/src/index.js
+++ react-shopping-cart/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ProductsServiceProvider } from "./components/products-service-context";
+import ProductService from "./services/productService";
+
+const productService = new ProductService();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProductsServiceProvider value={productService}>
+      <App />
+    </ProductsServiceProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
